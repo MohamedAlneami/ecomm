764bd00d1c8db6d71b57bbd47bdbb9e5d6715303
mohamed albadawi <mohabad@gmail.com>
2019-02-05 08:19:38 +0300

    add Worldwide Trademark

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 8000
       })
+
+      $('[data-add-to-cart]').click(function(e){
+          alert ("اضيف المنتج الى عربة الشراء");
+          e.stopPropagation();
+      });
 });
