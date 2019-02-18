74640448b703dcac440f026e4059839cfa420045
mohamed albadawi <mohabad@gmail.com>
2019-02-18 11:32:04 +0300

    product page

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,8 @@ $(document).ready(function(){
           alert ("اضيف المنتج الى عربة الشراء");
           e.stopPropagation();
       });
+      $('.product-option input[type="radio"]').change(function() {
+        $(this).parents('.product-option').siblings().removeClass('active');
+        $(this).parents('.product-option').addClass('active');
+      })
 });
